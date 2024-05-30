890982e9fc508d301ca9d9893dbb072b8f8f2b99
Hugues <hugueshugues2002@gmail.com>
2024-05-30 22:51:34 +0200

    b

--- App/settings.py
+++ App/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read.env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -94,8 +94,8 @@ WSGI_APPLICATION = 'App.wsgi.application'
 #     #     }  
 #     # }
 # }
-DATABASES ={
-    'default':dj_database_url.parse(env('DATABASE_URL'))
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
